e86780562f9249099dbb13e4d27f5432d44a3196
Olha Obertas <olhaobertas@gmail.com>
2020-05-22 16:16:57 -0400

    Homework7

--- src/main/java/hw7/work.java
+++ src/main/java/hw7/work.java
@@ -8,13 +8,15 @@ public class work {
         System.out.println("The statement that "+q+" is an odd number is "+isOdd(q));
     }
 
-
-    public static int Sum(int a, int b) {
-        return a + b;
-    }
+    //1
     public static void sum(int x, int y){
         System.out.println("The sum of "+x+" and "+y+" equals "+(x+y));
     }
+    //2
+    public static int Sum(int a, int b) {
+        return a + b;
+    }
+    //3
     public static boolean isOdd(int z){
         if (z%2==0) return true;
         else return false;
