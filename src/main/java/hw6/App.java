0f3b83ed80d78673c697819be59eb6f58a378fab
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-05-23 16:01:27 -0400

    HW6

--- src/main/java/hw6/App.java
+++ src/main/java/hw6/App.java
@@ -1,4 +1,23 @@
 package hw6;
 
 public class App {
+    public static void main(String[] args) {
+        sum(58,-36);
+        int gg=sum2(85,17);
+        System.out.println(gg);
+
+        System.out.println(isEven(18));
+
+    }
+    public static void sum(int a, int b){
+        System.out.println(a+b);
+    }
+    public static int sum2(int a, int b){
+        return a+b;
+    }
+
+    public static boolean isEven(int a){
+        return a % 2 == 0;
+    }
+
 }
