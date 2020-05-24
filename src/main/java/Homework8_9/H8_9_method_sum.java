ff74d8592d16889c56b85e617d4003825c861821
Michael Krasickiy <krasickiymichael@gmail.com>
2020-05-24 23:14:31 +0300

    Homework8_9

---
+++ src/main/java/Homework8_9/H8_9_method_sum.java
@@ -0,0 +1,13 @@
+package Homework8_9;
+
+public class H8_9_method_sum {
+    public static void main(String[] args) {
+        int yy = sum_of_6_numbers(2,3,5,6,7,8);
+        System.out.println(yy);
+
+    }
+
+    public static int sum_of_6_numbers(int a, int b, int c, int d, int e, int f){
+        return a+b+c+d+e+f;
+    }
+}
