a505298afe862a2dd3b01bf4d489246ffb6de782
Michael Krasickiy <krasickiymichael@gmail.com>
2020-05-24 23:13:23 +0300

    Homework8_9

---
+++ src/main/java/Homework8_9/H8_9_method_max.java
@@ -0,0 +1,29 @@
+package Homework8_9;
+
+public class H8_9_method_max {
+    public static void main(String[] args) {
+        int[] zz ={3,9,1,8,4,8,5};
+        System.out.println(max(zz));
+        System.out.println(min(zz));
+    }
+
+    public static int max(int[]zz) {
+        int max=zz[0];
+        for (int v:zz) {
+            if (v>max) {
+                max=v;
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[]zz) {
+        int min=zz[0];
+        for (int v:zz) {
+            if (v<min) {
+                min=v;
+            }
+        }
+        return min;
+    }
+}
