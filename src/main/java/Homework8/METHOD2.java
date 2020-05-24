ea69412e84c8e02876c50c6b473da5e9ef0e4f25
Irina Sidorova <65634978+IrinaS2020@users.noreply.github.com>
2020-05-23 23:41:11 -0600

    HW8

--- src/main/java/Homework8/METHOD2.java
+++ src/main/java/Homework8/METHOD2.java
@@ -1,16 +1,15 @@
 package Homework8;
 
-import java.util.Arrays;
-
 public class METHOD2 {
     public static void main(String[] args) {
         int[] array = {3, 9, 0, 8, 4, 8, 5};
-        System.out.println(Arrays.toString(sort(array)));
-        System.out.println(Arrays.toString(sort2(array)));
+        sort(array);
+        System.out.println("\n");
+        sort2(array);
     }
 
-    public static int[] sort(int[] xx) {
-        for (int ind = 0; ind < xx.length; ind++) {
+    public static void sort(int[] xx) {
+        for (int ind = 1; ind < xx.length; ind++) {
             int value = xx[ind];
             int i = ind - 1;
             for (; i >= 0; i--) {
@@ -20,11 +19,13 @@ public class METHOD2 {
             }
             xx[i + 1] = value;
         }
-        return xx;
+        for (int i : xx) {
+            System.out.print(" " + i);
+        }
     }
 
-    public static int[] sort2(int[] xx) {
-        for (int ind = 0; ind < xx.length; ind++) {
+    public static void sort2(int[] xx) {
+        for (int ind = 1; ind < xx.length; ind++) {
             int value = xx[ind];
             int i = ind - 1;
             for (; i >= 0; i--) {
@@ -34,6 +35,8 @@ public class METHOD2 {
             }
             xx[i + 1] = value;
         }
-        return xx;
+        for (int i : xx) {
+            System.out.print(" " + i);
+        }
     }
 }
