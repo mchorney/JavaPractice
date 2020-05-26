596f3593bdb6cd73b2dc8f94c95108f8df0a3b6d
David Katsal <katsaldavid@yahoo.com>
2020-05-25 20:38:21 -0700

    HW7

--- src/main/java/HW7/sumOfTwoNum_Void.java
+++ src/main/java/HW7/sumOfTwoNum_Void.java
@@ -7,5 +7,5 @@ public class sumOfTwoNum_Void {
     }
     //1. Создайте метод sum - который в качестве аргумента берет 2 целых числа и печатает их результат на экране.
 //вызовите этот метод из функции main() (подсказка - тип функции будет void)
-    public static void sumOfTwoNumbers(Integer numbers){System.out.println(" Sum = " +numbers);}
+    public static void sumOfTwoNumbers(Integer numbers){System.out.println("Sum = " +numbers);}
 }
--- src/main/java/HW7/work.java
+++ src/main/java/HW7/work.java
@@ -2,6 +2,35 @@ package HW7;
 
 public class work {
     public static void main(String[] args) {
+        sumOfTwoNumbers(78 + 13);
+
+        System.out.println(sumOfTwoNumbers(1, 7));
+
+        int odd =3;
+        int even =4;
+        System.out.println(isOdd(9));
 
     }
+
+    public static void sumOfTwoNumbers(Integer numbers) {
+        System.out.println("Sum = " + numbers);
+
+    }
+    public static int sumOfTwoNumbers(int a, int b) {
+        int d = a + b;
+        return d;
+
+    }
+    public static boolean isOdd(int number) {
+        boolean f = true || false;
+        if (number % 2 == 0) {
+            f = false;
+        }
+        if (number % 2!= 0) {
+            f = true;
+        }
+        return f;
+    }
+
 }
+
