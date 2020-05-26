3c4ee96d33a340ac9c8f9f7a6f5fb4aae5a6ce88
Alina_K <alinkagar@gmail.com>
2020-05-26 10:50:51 -0400

    HW8-9

---
+++ src/main/java/hw89/work.java
@@ -0,0 +1,71 @@
+package hw89;
+
+import java.awt.print.Book;
+
+public class work {
+    public static void main(String[] args) {
+        Book Chernovik = new Book();
+
+//        int [] array = {2,3,5,6,7,8};
+//        System.out.println(sum (array));
+
+        int[] unsorted = {3, 9, 1, 8, 4, 8, 5};
+
+
+//        Arrays.sort(unsorted);
+//        System.out.println(Arrays.toString(unsorted));
+//
+//        sort(unsorted);
+//        printarrey(unsorted);
+//
+//        }
+
+        System.out.println(max_from_array(unsorted));
+
+
+    }
+
+    public static int sum(int[] a) {
+        int start = 0;
+        for (int v : a) {
+            start = start + v;
+
+        }
+        return start;
+
+    }
+
+    public static void sort (int[]arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }}
+        public static void printarrey (int[] arr){
+            for (int v:arr) {
+                System.out.println(v);
+            }
+
+
+    }
+
+    public static int max_from_array(int[] xx) {
+        int max = xx[0];
+        for (int v : xx) {
+            if (v > max) {
+                max = v;
+            }
+
+        }
+        return max;
+    }
+}
+
+
+
+
+
