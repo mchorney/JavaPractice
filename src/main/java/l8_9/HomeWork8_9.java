b3ec9bdf2d757e294b8f31c66df276d1c46c99e5
Lyuba Balynska <lubochkab22@gmail.com>
2020-05-25 22:26:02 -0500

    HomeWork8_9

--- src/main/java/l8_9/HomeWork8_9.java
+++ src/main/java/l8_9/HomeWork8_9.java
@@ -8,52 +8,48 @@ public class HomeWork8_9 {
     public static void main(String[] args) {
         int[] arr1 = {2, 3, 5, 6, 7, 8};
         int[] arr2 = {3, 9, 1, 8, 4, 8, 5};
-        System.out.println(max(arr1));
-        System.out.println(max(arr2));
-        System.out.println(min(arr1));
-        System.out.println(min(arr2));
-//    }
-//        int[] cc = sort(arr1);
-//        for (int v : cc) {
-//            System.out.println(v);
-//        }
-        System.out.println(sum(arr1));
-        System.out.println(sum(arr2));
+        sort(arr2);
+        System.out.println("Max arr1 - "+max(arr1));
+        System.out.println("Max arr2 - "+max(arr2));
+        System.out.println("Min arr1 - "+min(arr1));
+        System.out.println("Min arr2 - "+min(arr2));
+        System.out.println("Summa arr1 - "+sum(arr1));
+        System.out.println("Summa arr2 - "+sum(arr2));
     }
-    public static int sum(int[] arr){
-        int sum=0;
-        for (int v:arr){
-              sum+=v;}
-            return sum;
-        }
-        public static int max(int[] arr){
-            int max=arr[0];
-            for (int v:arr){
-               if(v>max){
-                   max=v;
-               }
-            }
-            return max;
-        }
-        public static int min(int[] arr){
-        int min=arr[0];
-            for (int v:arr){
-                if(v<min){
-                    min=v;
-                }
-            }
-            return min;
-        }
 
-//         public static int[] sort(int[] arr){
-//            int[] new_arr = new int[arr.length];
-//            int max = arr[0];
-//            for (int i = 0;i < arr.length;i++) {
-//                if(arr[]>=max){
-//                    max=new_arr[arr.length-1];
-//                }
-//            }
-//            return new_arr;
-//        }
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int v : arr) {
+            sum += v;
+        }
+        return sum;
     }
+
+    public static int max(int[] arr) {
+        int max = arr[0];
+        for (int v : arr) {
+            if (v > max) {
+                max = v;
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] arr) {
+        int min = arr[0];
+        for (int v : arr) {
+            if (v < min) {
+                min = v;
+            }
+        }
+        return min;
+    }
+
+    public static void sort(int[] arr) {
+        Arrays.sort(arr);
+        for (int v : arr) {
+            System.out.println(v);
+        }
+    }
+}
 
