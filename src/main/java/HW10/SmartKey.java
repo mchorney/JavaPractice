b40de95b5749875a20449026a5dda6ca57ce44e0
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2020-05-27 18:44:31 -0700

    Home Work # 8-9 -- 5/27/2020; Version 2

--- src/main/java/HW10/SmartKey.java
+++ src/main/java/HW10/SmartKey.java
@@ -1,4 +1,17 @@
 package HW10;
 
 public class SmartKey {
+    String firstName;
+    String lastName;
+    boolean membershipStatus;
+
+    boolean areYouMember(SmartKey a) {
+        return a.membershipStatus;
+    }
+    void welcome(SmartKey a) {
+        if (a.areYouMember(a)) {
+            System.out.println(firstName + " " + lastName + " you are welcome!");
+        }else
+            System.out.println("Sorry! " + firstName  + " " + lastName +  " Access is denied");
+    }
 }
