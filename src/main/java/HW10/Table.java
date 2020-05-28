a32aba3b6373f482ef6e1fe473fca0ffbcbc9f34
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2020-05-28 10:08:00 -0700

    HW8-9 -- 5/28/2020; Version 3

--- src/main/java/HW10/Table.java
+++ src/main/java/HW10/Table.java
@@ -9,11 +9,11 @@ public class Table {
 
 
     void isOrderAccepted(Table a) {
-        if (desktop < 1 || legs < 4) {
+        if (a.desktop < 1 || a.legs < 4) {
             System.out.println("We need at least one desktop and at least four legs.");
         } else System.out.println("We are ready to place you order");
     }
     void yourOrder(Table a) {
-        System.out.println("Your order is " + material + " table with " + legs + " legs " + desktop + " desktop and " + shelves + " shelves." );
+        System.out.println("Your order is " + a.material + " table with " + a.legs + " legs " + a.desktop + " desktop and " + shelves + " shelves." );
     }
 }
