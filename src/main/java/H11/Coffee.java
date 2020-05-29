3500b4ab0e2f1437029c6ac4b2a09eaa39c3b807
Michael Krasickiy <krasickiymichael@gmail.com>
2020-05-30 00:24:50 +0300

    H11

---
+++ src/main/java/H11/Coffee.java
@@ -0,0 +1,27 @@
+package H11;
+
+public class Coffee {
+    private String name;
+    private String[] size;
+
+    public Coffee(String name, String[] size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(String[] size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getSize() {
+        return size;
+    }
+}
--- src/main/java/l11_1/App.java
+++ src/main/java/l11_1/App.java
@@ -3,16 +3,30 @@ package l11_1;
 public class App {
     public static void main(String[] args) {
         Person anna = new Person();
-        anna.name ="Anna";
+        anna.name = "Anna";
         anna.lastName = "Petrova";
         anna.year = 2000;
-        anna.prinyOut();
+        anna.printOut();
 
-        Person peter = new Person( "Peter",  "Jakobson",  1998);
-        peter.prinyOut();
-
-        Dog sharik = new Dog( "Sharik", "Dvorterier", 3);
+        Dog sharik = new Dog("Sharik", "Dvorterier", 3);
         sharik.printOut();
+        Dog mimi = new Dog();
+        mimi.printOut();
 
+        Cat cat = new Cat("Cat");
+
+        Car mazda = new Car("Mazda", "CX7", 2020);
+        mazda.setModel("CX9");
+        mazda.setYear(2023);
+        System.out.println(mazda.toString());
+
+        String[] col = {"Green", "Red", "Black"};
+        Snickers nikeWonder = new Snickers("Wonder", col);
+
+        String[] c = nikeWonder.getColor();
+        for (String v : c) {
+            System.out.println(v);
+
+        }
     }
 }
