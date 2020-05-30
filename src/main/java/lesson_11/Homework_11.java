b1bee8af7d749ebe18a5f774f9fe08ac8693c876
Olga Pestova <pestova.ov@gmail.com>
2020-05-30 00:51:04 -0600

    hw11

---
+++ src/main/java/lesson_11/Homework_11.java
@@ -0,0 +1,54 @@
+package lesson_11;
+//Сделать 3 класса - в каждом классе по краней мере 3 fields
+//Каждый аттрибут имеет setter и getter
+//Каждый класс имеет по краней мере 2 конструктора - default и полный (можно больше конструкторов)
+//создайте в каждом классе метод printClass() - для вывода всех полей на экран
+//По крайней мере один атрибут в одном классе должен быть array
+//Создайте в классе Work / main() по крайней мере 2 объекта каждого класса - один полным конструктором, один дефолтовым. Объект созданный дефолтовым конструктором заполните при помощи сеттеров
+//Выведите на экран эти объекты функцией printClass()
+public class Homework_11 {
+    public static void main(String[] args) {
+
+        String[][] n = {
+                {"Serving size = ", "Calories = ", "Fat = ", "Carbs = ", "Protein = "},
+                {"100", "335", "15", "47", "4"}
+        };
+        Cake black_forest = new Cake("Black Forest", "12 servings", n);
+
+        black_forest.printOut();
+        System.out.println("-------------------------------------");
+        Cake sacher = new Cake();
+        sacher.setName("Sacher");
+        sacher.setSize("10 servings");
+        String[][] n1 = {
+                {"Serving size = ", "Calories = ", "Fat = ", "Carbs = ", "Protein = "},
+                {"141", "420", "18", "62", "6"}
+        };
+        sacher.setNutrition_value(n1);
+        sacher.printOut();
+
+        System.out.println("**************************************");
+        String[] n2 = {"Jan-Feb","Mar-Apr","May-Jun","Jul-Aug","Sep-Oct","Nov-Dec"};
+        Magazine architectural_digest = new Magazine("Architectural Digest", "Interior design", n2);
+        architectural_digest.printOut();
+        System.out.println("-------------------------------------");
+        Magazine cosmopolitan = new Magazine();
+        cosmopolitan.setName("Cosmopolitan");
+        cosmopolitan.setCategory("Fasion and Entertainment");
+        String[] n3 = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+        cosmopolitan.setPublication_frequency(n3);
+        cosmopolitan.printOut();
+
+        System.out.println("**************************************");
+        String[] n4 = {"Two Bedroom Component Suite", "One Bedroom Suite", "Junior Suite", "Business Class Room", "Standard Room", "Economy Room"};
+        Hotel radisson = new Hotel("Radisson", "Costa Rica", n4);
+        radisson.printOut();
+        System.out.println("-------------------------------------");
+        Hotel hilton = new Hotel();
+        hilton.setName("Hilton");
+        hilton.setCountry("USA");
+        String[] n5 = {"STUDIO 1 KING BED", "2 KING 2 BEDROOM SUITE WITH SOFABED", "3 KING 3 BEDROOM PREMIER SUITE WITH SOFABED"};
+        hilton.setRoom_category(n5);
+        hilton.printOut();
+    }
+}
---
+++ src/main/java/lesson_11/Magazine.java
@@ -0,0 +1,46 @@
+package lesson_11;
+
+public class Magazine {
+    private String name;
+    private String category;
+    private String[] publication_frequency;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String[] getPublication_frequency() {
+        return publication_frequency;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPublication_frequency(String[] publication_frequency) {
+        this.publication_frequency = publication_frequency;
+    }
+
+    public Magazine(String name, String category, String[] publication_frequency) {
+        this.name = name;
+        this.category = category;
+        this.publication_frequency = publication_frequency;
+    }
+    public Magazine(){}
+
+    public void printOut(){
+        System.out.println(name+ "\n" +category);
+        String[] array = publication_frequency;
+        for (String v:array){
+            System.out.println(v);
+        }
+    }
+}
