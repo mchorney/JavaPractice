32eba1396a2e0c14ab4c5176811b2c543d8dd39b
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-06-03 00:07:38 -0700

    method showStatement overrided

--- src/main/java/homework14/myPayPalAccount.java
+++ src/main/java/homework14/myPayPalAccount.java
@@ -2,6 +2,7 @@ package homework14;
 
 public class myPayPalAccount extends myBankAccount {
     private int[] persons_list;
+    private String[] countries_list;
 
     public myPayPalAccount(String name, String last_name, String address, String phone, String email, double balance, int account_number, int[] persons_list) {
         super(name, last_name, address, phone, email, balance, account_number);
@@ -15,8 +16,9 @@ public class myPayPalAccount extends myBankAccount {
     public void setPersons_list(int[] persons_list) {
         this.persons_list = persons_list;
     }
-
+    @Override
     public void showStatement() {
         System.out.println("Dear " + name + " " + last_name + "!" + "\n You have on your PalPal account: " + balance + "$");
     }
+
 }
