8c3791278828ad5e288f09f3b29db817cc1eca1d
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-06-03 00:14:24 -0700

    methods added

--- src/main/java/homework14/myPayPalAccount.java
+++ src/main/java/homework14/myPayPalAccount.java
@@ -1,24 +1,13 @@
 package homework14;
 
 public class myPayPalAccount extends myBankAccount {
-    private int[] persons_list;
-    private String[] countries_list;
 
-    public myPayPalAccount(String name, String last_name, String address, String phone, String email, double balance, int account_number, int[] persons_list) {
+    public myPayPalAccount(String name, String last_name, String address, String phone, String email, double balance, int account_number) {
         super(name, last_name, address, phone, email, balance, account_number);
-        this.persons_list = persons_list;
     }
 
-    public int[] getPersons_list() {
-        return persons_list;
-    }
-
-    public void setPersons_list(int[] persons_list) {
-        this.persons_list = persons_list;
-    }
     @Override
     public void showStatement() {
         System.out.println("Dear " + name + " " + last_name + "!" + "\n You have on your PalPal account: " + balance + "$");
     }
-
 }
--- src/main/java/homework14/myWesternUnionAccount.java
+++ src/main/java/homework14/myWesternUnionAccount.java
@@ -1,4 +1,28 @@
 package homework14;
 
-public class myWesternUnionAccount {
+public class myWesternUnionAccount extends myPersonalAccount {
+    private int[] persons_list;
+    private String[] countries_list;
+
+    public myWesternUnionAccount(String name, String last_name, String address, String phone, String email, int[] persons_list, String[] countries_list) {
+        super(name, last_name, address, phone, email);
+        this.persons_list = persons_list;
+        this.countries_list = countries_list;
+    }
+
+    public int[] getPersons_list() {
+        return persons_list;
+    }
+
+    public void setPersons_list(int[] persons_list) {
+        this.persons_list = persons_list;
+    }
+
+    public String[] getCountries_list() {
+        return countries_list;
+    }
+
+    public void setCountries_list(String[] countries_list) {
+        this.countries_list = countries_list;
+    }
 }
