a740e5fc2b8bb01021b23d1af3e24afd53993f3f
Irina <prs.irina@gmail.com>
2020-06-13 16:08:53 -0600

    commit

---
+++ src/test/java/simpleTest1/UdemyTest.java
@@ -0,0 +1,31 @@
+package simpleTest1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UdemyTest {
+    @Test
+    public void udemyTest_ClickElephant_PageOpened() throws InterruptedException {
+
+        //Arrange
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://udemy.com");
+        Thread.sleep(2000);
+
+        //Act
+        WebElement elephant = driver.findElement(By.xpath("//*[contains(@src,'1242800')]"));
+        elephant.click();
+        String title = driver.getTitle();
+
+        //Assert
+        Assert.assertEquals(title,"The Complete SQL Bootcamp for the Manipulation and Analysis of Data | Udemy");
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
