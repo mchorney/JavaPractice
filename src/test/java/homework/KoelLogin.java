6adf66554277c71a8cdacc29bfa67008d89dcfd7
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-06-14 11:18:45 -0700

    Koel login added

--- src/test/java/homework/KoelLogin.java
+++ src/test/java/homework/KoelLogin.java
@@ -7,28 +7,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.List;
 
 public class KoelLogin {
-    @Test
-    public void LoginWithValidCredentials() throws InterruptedException {
-        //Arrange
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://koelapp.testpro.io/");
-        Thread.sleep(1000);
-        driver.manage().window().maximize();
-        //Act
-        WebElement email_field = driver.findElement(By.xpath("//*[@type='email']"));
-        email_field.sendKeys("koeluser21@testpro.io");
-        WebElement password_field = driver.findElement(By.xpath("//*[@type='password']"));
-        password_field.sendKeys("te$t$tudent");
-        WebElement login_button = driver.findElement(By.xpath("//*[@type='submit']"));
-        login_button.sendKeys(Keys.ENTER);
-        //Assertion
-        String title = driver.getTitle();
-        Assert.assertEquals(title, "Koel");
-        driver.quit();
-    }
+//    @Test
+//    public void LoginWithValidCredentials() throws InterruptedException {
+//        //Arrange
+//        System.setProperty("webdriver.chrome.driver", "chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://koelapp.testpro.io/");
+//        Thread.sleep(1000);
+//        driver.manage().window().maximize();
+//        //Act
+//        WebElement email_field = driver.findElement(By.xpath("//*[@type='email']"));
+//        email_field.sendKeys("koeluser21@testpro.io");
+//        WebElement password_field = driver.findElement(By.xpath("//*[@type='password']"));
+//        password_field.sendKeys("te$t$tudent");
+//        WebElement login_button = driver.findElement(By.xpath("//*[@type='submit']"));
+//        login_button.sendKeys(Keys.ENTER);
+//        //Assertion
+//        String title = driver.getTitle();
+//        Assert.assertEquals(title, "Koel");
+//        driver.quit();
+//    }
     @Test
     public void LoginWithInvalidCredentials() throws InterruptedException {
         //Arrange
@@ -44,9 +45,16 @@ public class KoelLogin {
         WebElement login_button = driver.findElement(By.xpath("//*[@type='submit']"));
         login_button.sendKeys(Keys.ENTER);
         //Assertion
-        WebElement element = driver.findElement(By.xpath("//*[@class='error']"));
-        System.out.println(element);
-        //Assert.assertTrue(element.isDisplayed());
+
+        // ? почему-то не могу достать class="error" value - и с использованием ClassName, и с Xpath
+        //List<WebElement> errors_elements = driver.findElements(By.xpath("//*[@class='error']"));
+        List<WebElement> errors_elements = driver.findElements(By.className("error"));
+        System.out.println(errors_elements.size());
+        Assert.assertEquals(errors_elements.size(), 1);
+
+        // ? так просто не находит элемент
+        //WebElement element1 = driver.findElement(By.className("error"));
+        //System.out.println(element1);
         driver.quit();
     }
 }
