cd62873c33c107abe1411f28c4cb2a0a626314e3
acrotruker <luchkovalerii@gmail.com>
2020-06-15 03:21:50 -0400

    Shw Koel login

---
+++ src/test/java/Shw2_3/Koel_login.java
@@ -0,0 +1,68 @@
+package Shw2_3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Koel_login {
+    @Test
+    public void Koel_Login() throws InterruptedException {
+        //Arrange
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://koelapp.testpro.io/#!/home");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        //Act
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/input[1]"));
+        email.sendKeys("testpro.user03@testpro.io");
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/input[2]"));
+        password.sendKeys("te$t$tudent");
+
+        WebElement loginBotton = driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/button"));
+
+        loginBotton.click();
+
+        String title = driver.getTitle();
+
+        //Assert
+        Assert.assertEquals(title, "Koel");
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+
+    @Test
+    public void Koel__WrongCredantials_Login_() throws InterruptedException {
+        //Arrange
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://koelapp.testpro.io/#!/home");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        //Act
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/input[1]"));
+        email.sendKeys("testpro.user03@testproi.io");
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/input[2]"));
+        password.sendKeys("te$t$tudent");
+
+        WebElement loginBotton = driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/button"));
+
+        loginBotton.click();
+
+        WebElement redSquare = driver.findElement(By.xpath("//*[@id=\"app\"]/div/form"));
+
+        //Assert
+        redSquare.isDisplayed();
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
