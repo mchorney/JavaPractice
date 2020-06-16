5434338eb1d4f5eef18c6f2ec2c9734f8086f9f7
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2020-06-16 12:25:01 -0700

    HW21 -- 6/16/2020; Version 2

---
+++ src/test/java/hw21/SeleniumTestFirst.java
@@ -0,0 +1,53 @@
+package hw21;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SeleniumTestFirst {
+    @Test
+    public void loginToKoelappSuccess() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://koelapp.testpro.io/#!/home");
+        Thread.sleep(3000);
+        WebElement emailAddress = driver.findElement(By.xpath("//*[@type='email']"));
+        emailAddress.sendKeys("koeluser03@testpro.io");
+        WebElement password = driver.findElement(By.xpath("//*[@type='password']"));
+        password.sendKeys("te$t$tudent");
+        WebElement loginButton = driver.findElement(By.xpath("//*[@type='submit']"));
+        loginButton.click();
+        Thread.sleep(3000);
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Koel");
+        Thread.sleep(3000);
+        driver.quit();
+        System.out.println("Test is passed");
+    }
+    @Test
+    public void loginToKoelappUnSuccessful() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://koelapp.testpro.io/#!/home");
+        Thread.sleep(3000);
+
+        String loginFormFrame = driver.findElement(By.xpath("//form[@data-v-e0457900]")).getAttribute("class");
+
+        WebElement emailAddress = driver.findElement(By.xpath("//*[@type='email']"));
+        emailAddress.sendKeys("koeluser03@testpro.io");
+        WebElement password = driver.findElement(By.xpath("//*[@type='password']"));
+        password.sendKeys("qwerty");
+        Thread.sleep(3000);
+        WebElement loginButton = driver.findElement(By.xpath("//*[@type='submit']"));
+        loginButton.click();
+        Thread.sleep(3000);
+
+        String loginFormFrameRed = driver.findElement(By.xpath("//form[@data-v-e0457900]")).getAttribute("class");
+
+        Assert.assertNotEquals(loginFormFrame, loginFormFrameRed);
+
+        driver.quit();
+        System.out.println("Test is passed");
+    }
+}
