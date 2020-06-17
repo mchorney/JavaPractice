514a76a5652bb4bba6b41b7b6b51422c4635f7b3
rusanaklt <rusana.klt@gmail.com>
2020-06-16 17:41:51 -0700

    Homework 21-22

--- src/test/java/UnitTests/hwk21.java
+++ src/test/java/UnitTests/hwk21.java
@@ -1,34 +1,30 @@
 package UnitTests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Base64;
+
 public class hwk21 {
     @Test
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\Program Files\\Selenium\\browser drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://koelapp.testpro.io");
         Thread.sleep(2000);
-        WebElement klass = driver.findElement(By.xpath("//div[@class='login-wrapper']/form"));
-
-        Assert.assertNotEquals(klass.getCssValue("border-color"), "rgb(142, 73, 71)");
-        Assert.assertNotEquals(klass.getAttribute("class"), "error");
 
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("koeluser03@testpro.io");
-        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("te$t1ng$tudent");
+        //driver.findElement(By.xpath("//input[@type='password']")).sendKeys("te$t$tudent");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(new String (Base64.getDecoder().decode("dGUkdCR0dWRlbnQ=")));
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         Thread.sleep(2000);
 
-        Assert.assertEquals(klass.getCssValue("border-color"), "rgb(142, 73, 71)");
-        Assert.assertEquals(klass.getAttribute("class"), "error");
-        Thread.sleep(2000);
-
+        Assert.assertEquals(driver.getCurrentUrl(), "https://koelapp.testpro.io/#!/home");
         driver.quit();
     }
 }
