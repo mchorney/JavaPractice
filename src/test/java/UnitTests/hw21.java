bf62d869c31d9783382f7184774101b940dd23c5
SergeiK <s.khatskevitch@gmail.com>
2020-06-16 19:53:18 -0600

    Home work 21

--- src/test/java/UnitTests/hw21.java
+++ src/test/java/UnitTests/hw21.java
@@ -5,25 +5,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class hw21 {
+   private WebElement error1;
+
    @Test
-    public void simpleTest2() throws InterruptedException {
-       System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-       WebDriver driver = new ChromeDriver();
-       driver.get("https://koelapp.testpro.io/");
-       Thread.sleep(2000);
-       WebElement email = driver.findElement(By.xpath("//input[@type='email']"));
-       email.sendKeys("testpro.user03@testpro.io");
-       WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
-       password.sendKeys("te$t$tudent");
-       WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
-       loginButton.click();
-       Thread.sleep(7000);
-//       WebElement logOut = driver.findElement(By.xpath("//i[@class='fa fa-sign-out control']"));
-//       loginButton.click();
-//       Thread.sleep(7000);
-       driver.quit();
+   public void simpleTest3() throws InterruptedException {
+      System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+      WebDriver driver = new ChromeDriver();
+      driver.get("https://koelapp.testpro.io/");
+      Thread.sleep(2000);
+      WebElement email = driver.findElement(By.xpath("//input[@type='email']"));
+      email.sendKeys("testpro.user03@testpro.io");
+      WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
+      password.sendKeys("te$t$tudent");
+      WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+      loginButton.click();
+      Thread.sleep(7000);
+//        WebElement logOut = driver.findElement(By.xpath("//i[@class='fa fa-sign-out control']"));
+//        loginButton.click();
+//        Thread.sleep(7000);
+      //try{
+//            xx.get(5);
+//            //throw ex;
+//        } catch (Exception ex){
+//            System.out.println(ex);
+//        } finally {
+//            System.out.printf("anyway");
+//        }
+      boolean result = true;
+      try {
+         error1 = driver.findElement(By.xpath("//*[@class='error']"));
+         result = false;
+      } catch (Exception ex){
+         System.out.println("Loged in");
+
+      }
+
+
+      Assert.assertEquals(result, true);
+
+      driver.quit();
    }
 }
