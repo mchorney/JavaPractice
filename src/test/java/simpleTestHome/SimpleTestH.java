6e2aa8fdffb5f701ad5bbf3b39ed9d4d8d7f06c7
Antanina Filimontsava <antaninafilimontsava@gmail.com>
2020-06-18 19:10:02 -0400

    SHome2_3cor

--- src/test/java/simpleTestHome/SimpleTestH.java
+++ src/test/java/simpleTestHome/SimpleTestH.java
@@ -7,17 +7,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleTestH {
+    private WebDriver driver;
+
+    @AfterMethod
+    public void tearDown()throws  InterruptedException{
+        Thread.sleep(5000);
+        driver.quit();
+    }
+    @BeforeMethod
+    public void startUp(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
 
     @Test
     public void simpleTest_Login() throws  InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
 
         driver.get("https://koelapp.testpro.io");
         Thread.sleep(2000);
@@ -31,29 +46,17 @@ public class SimpleTestH {
         WebElement loginSubmit = driver.findElement(By.xpath("//*[@type='submit']"));
         loginSubmit.click();
 
-        String title = driver.getTitle();
-        Assert.assertEquals(title, "Koel");
+        Thread.sleep(3000);
 
+        List<WebElement> list=new ArrayList<>();
+        list=driver.findElements(By.xpath("//*[@class='name']"));
 
-//        WebElement nameUser =driver.findElement(By.xpath("//*[@class='name']"));
-//        nameUser.isDisplayed();
-//        String url=driver.getCurrentUrl();
-//        Assert.assertEquals(url,"https://koelapp.testpro.io/#!/home");
-//        Assert.assertTrue(driver.findElements(By.xpath("//*[@class='name']")).contains("koeluser03"));
-
-
-        Thread.sleep(5000);
-        driver.quit();
+        Assert.assertNotEquals(list.size(),0);
 
     }
 
-
-
         @Test
-
         public void simpleTest_wrongEmail() throws  InterruptedException {
-            System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-            WebDriver driver=new ChromeDriver();
 
             driver.get("https://koelapp.testpro.io");
             Thread.sleep(2000);
@@ -66,18 +69,18 @@ public class SimpleTestH {
 
             WebElement loginSubmit=driver.findElement(By.xpath("//*[@type='submit']"));
             loginSubmit.click();
+            Thread.sleep(3000);
 
-            Assert.assertEquals(" //*[@class='error']"," //*[@class='error']");
+            List<WebElement> list=new ArrayList<>();
+            list=driver.findElements(By.className("error"));
+
+            Assert.assertEquals(list.size(),1);
+
+//            Assert.assertEquals(" //*[@class='error']"," //*[@class='error']");
 
 //            WebElement error=driver.findElement(By.xpath(" //*[@class='error']"));
 //            error.isDisplayed();
 
-
-
-
-
-            Thread.sleep(5000);
-            driver.quit();
 
     }
 }
