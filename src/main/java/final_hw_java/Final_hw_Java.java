e5700bef739d33426acbef8e793e98b39db501a7
Olga Pestova <pestova.ov@gmail.com>
2020-06-19 11:09:31 -0600

    ..

---
+++ src/main/java/final_hw_java/Final_hw_Java.java
@@ -0,0 +1,44 @@
+package final_hw_java;
+
+//Animal Kingdom
+//=======================
+//interface - Life
+//in Life
+//interface - Fungi
+//inrerface - Bacteria
+//interface - Plants
+//interface - Animals
+//in Animals
+//interface Vertebrates
+//interface Invertebrates
+//in Vertebrates
+//interface Reptilies
+//interface Fish
+//interface Amphibians
+//interface Birds
+//interface Mammals
+//in Mammals
+//abstract class Carnivores
+//abstract class Primates
+//abstract class Seals
+//abstract class Rodents
+//abstract class Whales
+//abstract class Herbivores
+//in Carnivores
+//class - Lion
+//class - Wolf
+//class - BlueWhale
+//придумайте по краней мере один метод для каждого интерфейса или абстрактного класса в иерархии начиная от Life и реализуйте их в трех классах, вызовите все методы
+//инстаншиируйте объекты всех трех классов;
+//Например:
+//interface Life{
+//    void canBreath();
+//}
+//в классе Lion этот метод можно реализовать так
+//public void canBreath(){
+// System.out.println("I can breath!!!!");
+//}
+
+public class Final_hw_Java {
+
+}
