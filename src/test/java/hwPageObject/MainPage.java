6cca5ad46f6516bd03907900edefbc5df2471219
Irena Shpetehaib <irenashap7@gmail.com>
2020-06-25 23:18:37 -0400

    hw 8

--- src/test/java/hwPageObject/MainPage.java
+++ src/test/java/hwPageObject/MainPage.java
@@ -3,12 +3,20 @@ package hwPageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
 
 public class MainPage {
     private WebDriver driver;
+    private FluentWait<WebDriver> fluentWait;
+
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
+        fluentWait = new FluentWait<>(driver)
+            .withTimeout(Duration.ofSeconds(20))
+            .pollingEvery(Duration.ofMillis(100));
     }
 
 
@@ -25,12 +33,11 @@ public class MainPage {
     }
 
 
-    public void createPlaylist(String Top10) {
+    public void createPlaylist(String name) {
 
-      getButtonCreatePlaylist().click();
-      getWindowToSave().click();
-      getClickOnTop10().click();
-
+        getButtonCreatePlaylist().click();
+        getWindowToSave().click();
+        getClickOnTop10().click();
 
 
 //        WebElement buttonWomen = driver.findElement(By.xpath("(//*[@href=\"http://automationpractice.com/index.php?id_category=3&controller=category\"])[2]"));
@@ -38,8 +45,9 @@ public class MainPage {
 
     }
 
-    public boolean checkPlaylist(String Top10) {
+    public boolean checkPlaylist(String name) {
         var isCheckPlaylist = driver.findElements(By.xpath("\"//*[@href=\\\"#!/playlist/4843\\\"]\""));
         return true;
     }
+
 }
