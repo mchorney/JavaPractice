ef0e4e58aca219d1fcc4b9e2220509f6582b3a55
Irena Shpetehaib <irenashap7@gmail.com>
2020-06-27 14:50:57 -0400

    hw 9

--- src/test/java/hwPageObject/LoginPage.java
+++ src/test/java/hwPageObject/LoginPage.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.FluentWait;
 
-import java.time.Duration;
 
 
 public class LoginPage extends BasePage {
--- src/test/java/hwPageObject/MainPage.java
+++ src/test/java/hwPageObject/MainPage.java
@@ -23,37 +23,7 @@ public class MainPage extends BasePage {
         String url = driver.getCurrentUrl();
         return url.split("/")[4];
     }
-//    Actions action = new Actions(driver);
-//
-////    WebElement rename = driver.findElement(By.xpath("//*[@href='#!/playlist/5125'"));
-//    WebElement renameWindow = driver.findElement(By.xpath("//*[@href='#!/playlist/5125'"));
-//
-//    public WebElement getReName_ClickOnTop10(){
-//        fluentWait.until(x->x.findElement(By.xpath("//*[@href='#!/playlist/5125'")).isDisplayed() );
-//        return driver.findElement(By.xpath("//*[@href='#!/playlist/5125'"));}
-//
-//        public void renameWindow(String name) {
-//            getReName_ClickOnTop10().click();
-//            action.doubleClick(renameWindow).build().perform();
-//        }
-//        public String ReName_ClickOnTop10(String name){
-//
-//        getReName_ClickOnTop10().click();
-//         action.doubleClick(renameWindow).build().perform();
-//
-//         String url = driver.getCurrentUrl();
-//         return url.split("/")[4];
-//
-//        }
-//
-//    public void setAction(Actions action) {
-//        this.action = action;
-//        action.doubleClick().build().perform();
-//    }
-//
-//    public Actions getAction() {
-//        return action;
-//    }
+
 
     public boolean checkPlaylist(String id) {
         var list = driver.findElements(By.xpath("//*[@href='#!/playlist/"+id+"']"));
