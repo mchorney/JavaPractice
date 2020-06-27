65526fbc500aeef066bf61692ec46e827bbeeb89
mchorney <m.chorney7@gmail.com>
2020-06-27 13:20:32 -0500

    shw6

--- src/test/java/pageObjects/MainPage.java
+++ src/test/java/pageObjects/MainPage.java
@@ -1,45 +1,64 @@
 package pageObjects;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
+
 
 public class MainPage {
+
     private WebDriver driver;
+    private WebElement getAddPlaylistButton() {
+        return driver.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']"));
+    }
+
+    private WebElement getPlaylistNameField() {
+        return driver.findElement(By.xpath("//*[@class='create'] //*[@type='text']"));
+    }
+
+    private WebElement getPlaylistName(String playlistName) {
+        return driver.findElement(By.xpath("//*[contains(text(),'"+playlistName+"')][1]"));
+    }
+
+    private WebElement getInputPlaylistName() {
+        return driver.findElement(By.xpath("//*[@class='active']"));
+    }
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
-
     public boolean isMain() {
-        var list = driver.findElements(By.cssSelector("[class='fa fa-sign-out control']"));
-        return list.size() == 1;
+        var list = driver.findElements(By.xpath("//*[@class='fa fa-sign-out control']"));
+        return list.size()==1;
     }
 
-    public void createPlaylist(String Playlist) {
-        getPlus().click();
-        entryField().sendKeys("MC_78", Keys.ENTER);
+    public void createPlaylist(String name) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(getAddPlaylistButton()).click().build().perform();
+        getAddPlaylistButton().click();
+        getPlaylistNameField().sendKeys(name);
+        getPlaylistNameField().sendKeys(Keys.ENTER);
     }
 
-    public boolean checkPlaylist(String xxxxx) {
-        try {
-            driver.findElement(By.xpath("//*[@href='#!/playlist/4885']"));
-        } catch (NoSuchElementException error) {
-            return false;
-        }
-        return true;
+    public boolean checkPlaylist(String name) {
+        var list = driver.findElements(By.linkText(name));
+        return (list.size()==1);
     }
-
-    public WebElement getPlus() {
-        return driver.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']"));
+    public void renamePlaylist(String oldName, String newName) {
+        Actions actions = new Actions(driver);
+        actions.doubleClick(getPlaylistName(oldName)).perform();
+        actions.click().keyDown(Keys.CONTROL).sendKeys(Keys.chord("a")).keyUp(Keys.CONTROL).perform();
+        actions.doubleClick().sendKeys(newName).sendKeys(Keys.ENTER).perform();
     }
-
-    public WebElement entryField() {
-        return driver.findElement(By.xpath("//*[@placeholder='↵ to save']"));
-    }
-
-    public WebElement playlist() {
-        return driver.findElement(By.xpath("//*[@href='#!/playlist/4885']"));
-    }
-
 
 }
--- src/test/java/seleniumTests/LoginTest.java
+++ src/test/java/seleniumTests/LoginTest.java
@@ -1,7 +1,11 @@
 package seleniumTests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,32 +17,47 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
     WebDriver driver;
+
     @BeforeMethod
-    public void startUp(){
+    public void strartUp() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get("https://koelapp.testpro.io");
+        driver.get("https://koelapp.testpro.io/");
     }
     @AfterMethod
     public void tearDown() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         driver.quit();
     }
-    @Test
-    public void loginTest_CorrectCredentials_LoggedToApp(){
 
+    @Test
+    public void loginTest_CorrectCredentials_loggedToApp() {
+        LoginPage loginPage = new LoginPage(driver);
+        MainPage mainPage = loginPage.loginToApp("testpro.user03@testpro.io", "te$t$tudent");
+        Assert.assertTrue(mainPage.isMain());
+    }
+    @Test
+    public void loginTest_IncorrectCredentials_loggedToApp() {
+        LoginPage loginPage = new LoginPage(driver);
+        boolean check = loginPage.isError("testpro.user03@testpro.io", "wrongPassword");
+        Assert.assertTrue(check);
+    }
+
+    @Test
+    public void playlistTests_createPlaylist_PlaylistCreated() {
         LoginPage loginPage = new LoginPage(driver);
         MainPage mainPage = loginPage.loginToApp("testpro.user03@testpro.io","te$t$tudent");
-        Assert.assertTrue(mainPage.isMain());
-
+        mainPage.createPlaylist("new new playlist");
+        Assert.assertTrue(mainPage.checkPlaylist("new new playlist"));
     }
+
     @Test
-    public void loginTest_WrongCredentials_Error(){
-
+    public void playlistTest_renamePlaylist_playlistRenamed() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.loginToApp("testpro.user03@testpro.io","1111111");
-        Assert.assertTrue(loginPage.isError());
-
+        MainPage mainPage = loginPage.loginToApp("testpro.user03@testpro.io","te$t$tudent");
+        mainPage.renamePlaylist("new new playlist", "NEW playlist ");
+        Assert.assertTrue(mainPage.checkPlaylist("NEW playlist"));
     }
+
 }
