d36b5a952bf626bf6f4e95f74d7185e0f7550069
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-07-01 18:05:53 -0700

    all tests passed in FireFox and Opera

--- src/test/java/SeleniumTests/BaseTest.java
+++ src/test/java/SeleniumTests/BaseTest.java
@@ -14,7 +14,7 @@ public class BaseTest {
 
     @BeforeMethod
     public void startUp() throws NoSuchFieldException {
-        driver = BrowserFabric.getDriver(Browsers.CHROME);
+        driver = BrowserFabric.getDriver(Browsers.OPERA);
         driver.get("https://koelapp.testpro.io/");
         LoginPage loginPage = new LoginPage(driver);
         mainPage = loginPage.loginToApp("koeluser21@testpro.io", "te$t$tudent");
