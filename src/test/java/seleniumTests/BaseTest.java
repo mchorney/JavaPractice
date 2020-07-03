bfc96930296463187b09a2a52e9baef76acc4218
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-07-02 22:05:13 -0400

    S10 - TestNG - P1

--- src/test/java/seleniumTests/BaseTest.java
+++ src/test/java/seleniumTests/BaseTest.java
@@ -16,7 +16,7 @@ public class BaseTest {
     @BeforeMethod
     public void startUp() throws NoSuchFieldException {
 
-        driver= BrowserFabric.getDriver(Browsers.FIREFOX);
+        driver= BrowserFabric.getDriver(Browsers.CHROME);
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
