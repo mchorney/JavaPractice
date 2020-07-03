a0f31a350813b773251151ae98f1ef03fa39b094
Michael Krasickiy <krasickiymichael@gmail.com>
2020-07-03 06:35:18 +0300

    Homeworks

---
+++ src/main/java/passenger_1/TicketPrice.java
@@ -0,0 +1,7 @@
+package passenger_1;
+
+public class TicketPrice {
+    public double getTicketPrice(int age, int price){
+      double
+    }
+}
--- src/test/java/seleniumTests/LoginTest.java
+++ src/test/java/seleniumTests/LoginTest.java
@@ -1,7 +1,9 @@
 package seleniumTests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import passenger_1.TicketPrice;
 
 public class LoginTest extends BaseTest {
 
@@ -24,5 +26,21 @@ public class LoginTest extends BaseTest {
     @Test
     public void loginTest_CorrectCredentials_LoggedToApp4(){
         Assert.assertTrue(mainPage.isMain());
+    }
+    @DataProvider(name = "Ages")
+    public Object[][] createData(){
+        return new Object[][]{
+                {1,1000,0.0},
+                {5,1000,500.0},
+                {15,1000,1000.0},
+                {68,1000,800.0}
+        };
+    }
+    @Test(dataProvider = "Ages")
+    public void ticketPriceCalculator(int age, int fare, double result){
+        TicketPrice calcuator= new TicketPrice();
+        double ticketPrice = calcuator.getTicketPrice(age, fare);
+        Assert.assertEquals(ticketPrice, result);
+
     }
 }
